ad7c1b2f7f705c69d6d7256eb74596a40ba17ce6
thanhlvcpy <42794074+thanhlvcpy@users.noreply.github.com>
2019-03-18 22:40:04 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,21 +21,13 @@ int main()
     for(long long i=1;i<=n;i++)
         cin>>cow[i].p>>cow[i].c;
     sort(cow+1,cow+n+1,cmp);
-    long long t=0;
     long long d=0;
-    long long id;
     for(int i=1;i<=n;i++)
     {
-        t=t+cow[i].p*cow[i].c;
-        d=d+cow[i].c;
-        id=i;
-        if(t>=b)
-            break;
-    }
-    while(t>b)
-    {
-        t=t-cow[id].p;
-        d--;
+        if (b <cow[i].p) break;
+        long long tmp = b / cow[i].p;
+        b =b - min(tmp,cow[i].c) * cow[i].p;
+		d =d + min(tmp,cow[i].c);
     }
     cout<<d;
     return 0;
